9e7be10ecbb7e1e85f963e268ca18646d5a35697
FlamingGenius <livewiretech.reply.mail@gmail.com>
2016-03-08 16:03:04 -0500

    Update loginForm.php

--- server/constants/loginForm.php
+++ server/constants/loginForm.php
@@ -1,1 +1,7 @@
+<form method="post">
+	<input type="text" name="username" placeholder="Username"> <br>
+    <input type="password" name="password" placeholder="Password"> <br>
+    <input type="submit" name="submit" value="Login">
+</form>
 
+<? require_once("server/handles/loginHandle.php"); ?>
