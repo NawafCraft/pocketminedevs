b03f0dcf2698bbffdfc1d1bb242a044d703f7416
FlamingGenius <livewiretech.reply.mail@gmail.com>
2016-03-08 15:22:36 -0500

    Create functions.js

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function Hide(id){
+	if(id.style.display == 'block'){
+		id.style.display = 'none';
+	}
+}
+
+function Show(id){
+	if(id.style.display == 'none'){
+		id.style.display = 'block';
+	}
+}
